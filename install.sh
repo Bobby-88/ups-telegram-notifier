cd659d2bcaa7a02f987e3fa2a1ff6cfc23ab10bb
Bobby-88 <vova.ilchenko@gmail.com>
2020-09-19 20:31:56 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,9 +8,9 @@ echo TELEGRAM_TOKEN="${tg_token}" > /etc/telegram.sh.conf
 echo TELEGRAM_CHAT="${tg_chat_id}" >> /etc/telegram.sh.conf
 mkdir -p /opt/tg-notifier
 cd /opt/tg-notifier
-curl -o telegram.sh https://raw.githubusercontent.com/Bobby-88/shell-tg-notifier/master/telegram.sh
+curl -o telegram.sh -fsSL https://raw.githubusercontent.com/Bobby-88/shell-tg-notifier/master/telegram.sh
 chmod +x telegram.sh
-curl -o power-down-tg.sh https://raw.githubusercontent.com/Bobby-88/shell-tg-notifier/master/power-down-tg.sh
+curl -o power-down-tg.sh -fsSL https://raw.githubusercontent.com/Bobby-88/shell-tg-notifier/master/power-down-tg.sh
 chmod +x power-down-tg.sh
-curl -o bypass-on-tg.sh https://raw.githubusercontent.com/Bobby-88/shell-tg-notifier/master/bypass-on-tg.sh
+curl -o bypass-on-tg.sh -fsSL https://raw.githubusercontent.com/Bobby-88/shell-tg-notifier/master/bypass-on-tg.sh
 chmod +x bypass-on-tg.sh
