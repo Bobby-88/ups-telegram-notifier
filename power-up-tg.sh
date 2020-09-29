3f089a12404c5db557cae355d4db32008842e8bd
Bobby-88 <vova.ilchenko@gmail.com>
2020-09-29 23:19:01 +0300

    changed path to telegram.sh after rename

--- power-up-tg.sh
+++ power-up-tg.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/tg-notifier/telegram.sh -T "✔️ [UPS_NAME] UPS" -M "*Input Power Recovered* - no longer on battery"
+/opt/ups-telegram-notifier/telegram.sh -T "✔️ [UPS_NAME] UPS" -M "*Input Power Recovered* - no longer on battery"
